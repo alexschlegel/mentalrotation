fd6560ce4b3b809d61007e3bf082db2070e2c3fd
Alex Schlegel <schlegel@gmail.com>
2015-03-23 16:17:40 -0400

    retooled Masks

---
+++ code/+MR/Masks.m
@@ -0,0 +1,31 @@
+function s = Masks()
+% MR.Masks
+% 
+% Description:	get a struct of mask names
+% 
+% Syntax:	s = MR.Masks()
+% 
+% Updated: 2015-03-23
+% Copyright 2015 Alex Schlegel (schlegel@gmail.com).  This work is licensed
+% under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
+% License.
+s.ci	=	{
+				'dlpfc'
+				'fef'
+				'ppc'
+				'pcu'
+				'loc'
+				'occ'
+			};
+
+s.motor	=	{
+				'pmv'
+				'pmd'
+				'sma'
+				'pre_sma'
+				'primary_motor'
+				'somatosensory'
+				'cerebellum'
+			};
+
+s.all	= [s.motor; s.ci];
